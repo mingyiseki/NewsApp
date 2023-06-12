421e3ece5d19402d819e002f826eefe0e0004f33
cc <cc1628404960@163.com>
2023-06-12 18:32:54 +0800

    实验五的第一部分

--- app/src/main/java/com/example/newsapp/receiver/MyReceiver.java
+++ app/src/main/java/com/example/newsapp/receiver/MyReceiver.java
@@ -4,11 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d("cc", "收到自定义广播");
         String customValue = intent.getStringExtra("extraKey");
         //收到指定广播
         Log.d("cc", customValue);
